408d0bafcc04087e9930e631e075660149d01e0e
Summer Wollin <summer.wollin@gmail.com>
2016-05-12 11:21:10 -0600

    api signup endpoint returns errors if blank fields

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/signup', function(req, res, next) {
+    const errors = [];
+
+    if (!req.body.email || !req.body.email.trim()) errors.push("Email can't be blank");
+    if (!req.body.name || !req.body.name.trim()) errors.push("Name can't be blank");
+    if (!req.body.password || !req.body.password.trim()) errors.push("Password can't be blank");
+
+    if (errors.length) {
+      res.status(422).json({
+        errors: errors
+      })
+    }
 });
 
 module.exports = router;
